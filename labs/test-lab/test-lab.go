500ac2bc5c5c647c7ad05cc5982c511607db7963
Marcelo Alvarez Garza <chelo39.alvarez@gmail.com>
2021-02-01 22:40:41 +0000

    Solve test-lab FINAL
    Signed-off-by: Marcelo Alvarez Garza <chelo39.alvarez@gmail.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -6,7 +6,6 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
 
 	name := ""
 	if len(os.Args) == 1 {
@@ -17,16 +16,4 @@ func main() {
 		}
 		fmt.Println(name + ", you have entered the Matrix. Welcome.")
 	}
-=======
-	
-	name := ""
-	if len(os.Args) == 1 {
-	  fmt.Println("Error")
-	}else {
-	  for _, word := range os.Args[1:]{
-	    name = fmt.Sprintf("%v %v ", name, word)
-	  }
-	 fmt.Println(name + ", you have entered the Matrix. Welcome.")
-  }
->>>>>>> 95826de17c2b4879da20ff3a9ae25fd6589b5e4e
 }
